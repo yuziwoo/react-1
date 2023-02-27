1045a0a0cbd31184e638958894348b3c13516f38
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-02-28 00:02:52 +0900

    baekjoon

--- test.js
+++
@@ -1,9 +0,0 @@
-s = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-t = s.shift();
-x = s.shift();
-b = 0;
-for (let i = 0; i< +x; i++) {
-  [y, z] = s[i].split(" ").map(v => +v);
-  b += y * z;
-}
-console.log(b == t ? "Yes" : "No");
