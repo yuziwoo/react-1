45b1b6b00ed198528ce41e2fc47f1be7a25d1477
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-03-02 13:00:09 +0900

    백준 푸는중

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-s
+a=0;b=0;p=['F','','D0','D+','C0','C+','B0','B+','A0','A+'];require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map((v)=>{d = v.split(' ')[2];if(d !='P'){c= +v.split(' ')[1];a += p.indexOf(d)*c/2;b += c;}});console.log((a/b).toFixed(6));
