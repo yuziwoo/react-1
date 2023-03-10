9b6369c98226817e867d3891346a855f7f7de639
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-03-10 21:24:41 +0900

    testing

--- test.js
+++ test.js
@@ -1,32 +1,11 @@
-let s = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-let res = [];
-let count = s.shift();
-
-for(let i=0; i<count; i++) {
-  let [x1, y1, r1, x2, y2, r2] = s[i].split(" ").map(Number);
-  let dis = 0;
-
-  if (x1 == x2 || y1 == y2) {
-    dis = Math.abs(x2 - x1) + Math.abs(y2 - y1);
-  } else {
-    dis = Math.sqrt(((x2 - x1) ** 2) + (y2 - y1) ** 2);
-  }
-
-  if (dis > r1 + r2) {
-    res.push(0);
-  } else if (dis == 0) {
-    if (r1 == r2) {
-      res.push(-1);
-    }else {
-      res.push(0);
-    }
-  } else if (dis < Math.abs(r2 - r1)) {
-    res.push(0);
-  }else if (dis == r1 + r2 || dis == Math.abs(r2 - r1)) {
-    res.push(1);
-  } else {
-    res.push(2);
-  }
+let s = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(v => parseInt(v, 2));
+let a = (s[0]+s[1]).toString(2);
+let b = 0;
+if (a[0] == 0) {
+    b = a.split("");
+    b.shift();
+    b = b.join("");
+}else {
+    b = a;
 }
-
-console.log(res.join("\n"));
+console.log(b);
