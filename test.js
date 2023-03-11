44df2fb68fe2d281bd112e067ccaa9ac82732455
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-03-11 09:45:49 +0900

    백준 수정

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-let s = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(v => parseInt(v, 2));
-let a = (s[0]+s[1]).toString(2);
-let b = 0;
-if (a[0] == 0) {
-    b = a.split("");
-    b.shift();
-    b = b.join("");
-}else {
-    b = a;
-}
-console.log(b);
+let [d,w,h] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(v => +v);
+let a = (x) => Math.floor(x*d/(h**2 + w**2)**0.5);
+console.log(a(h),a(w));
