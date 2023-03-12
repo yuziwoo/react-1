41113966830b61bc916b9ed4fec2ce1d38ab2eb9
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-03-12 22:59:05 +0900

    10951 푸ㄹ기

--- test.js
+++ test.js
@@ -1,3 +1,2 @@
-let [d,w,h] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(v => +v);
-let a = (x) => Math.floor(x*d/(h**2 + w**2)**0.5);
-console.log(a(h),a(w));
+let s = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(v => v.split(" "));
+let a = ""
